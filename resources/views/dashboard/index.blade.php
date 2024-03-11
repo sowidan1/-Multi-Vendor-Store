0bc34e79033bb697d6fd8b4d23b7c02d5d971df0
sowidan1 <osamasowidan1@gmail.com>
2024-03-11 23:54:36 +0200

    some edit

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,29 +1,9 @@
 @extends('layouts.dashboard')
 
-@section('content')
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Starter Page</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Starter Page</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+@section('title', 'Starter Page')
 
-    <!-- Main content -->
-    <div class="content">
-      <div class="container-fluid">
+@section('content')
+
         <div class="row">
           <div class="col-lg-6">
             <div class="card">
@@ -81,10 +61,5 @@
           </div>
           <!-- /.col-md-6 -->
         </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+
 @endsection
