61bbe1bc2e22c7b24283a47df34a7fc5dd67a8c7
sowidan1 <osamasowidan1@gmail.com>
2024-03-11 16:32:12 +0200

    roll

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [DashboardController::class, 'index']);
-Route::get('/', [DashboardController::class, 'index']);
-Route::get('/', [DashboardController::class, 'index']);
 
 
